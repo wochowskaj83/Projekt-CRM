e6599567b89a8e83ff3d1c122ba4a14cdc2c3ad4
wochowskaj83 <wochowskaj@gmail.com>
2023-11-03 10:43:37 +0100

    polaczenie z baza danych

---
+++ Backend/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    app: {
+        port:3000
+    },
+
+    db: {
+        host: '127.0.0.1',
+        port: 27017,
+        name: 'Projekt-CRM',
+        password: ''
+    }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,25 @@
+const config = require('./config')
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const mongoUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`
+
+mongoose
+    .connect(mongoUrl, {})
+    .then(() => {
+        console.log('MongoDB is connected')
+    })
+    .catch((err) => {
+        throw err
+    })
+
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.listen(7070, () => {
+app.listen(config.app.port, () => {
     console.log('serwer Node.js działa');
 });
