19ff6530c1c1a7f6019f41474715f10eeb571a89
wochowskaj83 <wochowskaj@gmail.com>
2023-11-07 20:35:29 +0100

    podstrony rejestracji i logowania

--- Frontend/src/views/Signup.js
+++ Frontend/src/views/Signup.js
@@ -1,5 +1,29 @@
+import './Signup.css'
+
 const Signup = () => {
-    <h2>Signup</h2>
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <input type="text" placeholder="Name" />
+                </div>
+                <div className="input">
+                    <input type="email" placeholder="Email"/>
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Password"/>
+                </div >
+            </div >
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+            </div>
+        </div >
+        
+    )
 }
 
 export default Signup;
